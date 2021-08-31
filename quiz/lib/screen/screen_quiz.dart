33e7c85a3e642b3dc052e91412c02433232390dc
LeeSeokJune <47683651+LeeSeokJune@users.noreply.github.com>
2021-08-31 17:42:31 +0900

    quiz

--- quiz/lib/screen/screen_quiz.dart
+++ quiz/lib/screen/screen_quiz.dart
@@ -4,23 +4,23 @@ import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:quiz/widget/widget_candidate.dart';
 
-class QuizScreen extends StatefulWidget {
-  List<Quiz> quizs;
-  QuizScreen({required this.quizs});
+import '/model/model_quiz.dart';
+import '/screen/screen_result.dart';
+import '/widget/widget_candidate.dart';
 
+class QuizScreen extends StatefulWidget {
+  List<Quiz>? quizs;
+  QuizScreen({this.quizs});
   @override
   _QuizScreenState createState() => _QuizScreenState();
 }
 
 class _QuizScreenState extends State<QuizScreen> {
-  List<int> _answer = [-1, -1, -1, -1];
-  List<bool> _answerState = [
-    false,
-    false,
-    false,
-    false,
-  ];
+  List<int> _answers = [-1, -1, -1];
+  List<bool> _answerState = [false, false, false, false];
   int _currentIndex = 0;
+  SwiperController _controller = SwiperController();
+
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
@@ -33,18 +33,17 @@ class _QuizScreenState extends State<QuizScreen> {
           child: Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
-              border: Border.all(
-                color: Colors.deepPurple,
-              ),
+              border: Border.all(color: Colors.deepPurple),
             ),
             width: width * 0.85,
             height: height * 0.5,
             child: Swiper(
+              controller: _controller,
               physics: NeverScrollableScrollPhysics(),
               loop: false,
-              itemCount: widget.quizs.length,
+              itemCount: widget.quizs!.length,
               itemBuilder: (BuildContext context, int index) {
-                return _buildQuizCard(widget.quizs[index], width, height);
+                return _buildQuizCard(widget.quizs![index], width, height);
               },
             ),
           ),
@@ -56,10 +55,9 @@ class _QuizScreenState extends State<QuizScreen> {
   Widget _buildQuizCard(Quiz quiz, double width, double height) {
     return Container(
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(color: Colors.white),
-        color: Colors.white,
-      ),
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Colors.white),
+          color: Colors.white),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
@@ -90,34 +88,73 @@ class _QuizScreenState extends State<QuizScreen> {
             child: Container(),
           ),
           Column(
-            children: _buildCandidates(width, quiz)!,
+            children: _buildCandidates(width, quiz),
           ),
+          Container(
+            padding: EdgeInsets.all(width * 0.024),
+            child: Center(
+              child: ButtonTheme(
+                minWidth: width * 0.5,
+                height: height * 0.05,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: RaisedButton(
+                  child: _currentIndex == widget.quizs!.length - 1
+                      ? Text('결과보기')
+                      : Text('다음문제'),
+                  textColor: Colors.white,
+                  color: Colors.deepPurple,
+                  onPressed: _answers[_currentIndex] == -1
+                      ? null
+                      : () {
+                          if (_currentIndex == widget.quizs!.length - 1) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ResultScreen(
+                                  answers: _answers,
+                                  quizs: widget.quizs,
+                                ),
+                              ),
+                            );
+                          } else {
+                            _answerState = [false, false, false, false];
+                            _currentIndex += 1;
+                            _controller.next();
+                          }
+                        },
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
   }
 
-  List<Widget>? _buildCandidates(double width, Quiz quiz) {
+  List<Widget> _buildCandidates(double width, Quiz quiz) {
     List<Widget> _children = [];
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
       _children.add(
         CandWidget(
-            index: i,
-            text: quiz.candidates![i],
-            width: width,
-            answerState: _answerState[i],
-            tap: () {
-              setState(() {
-                for (int j = 0; j < 3; j++) {
-                  if (j == i) {
-                    _answerState[j] = true;
-                    _answer[_currentIndex] = j;
-                  } else {
-                    _answerState[j] = false;
-                  }
+          index: i,
+          text: quiz.candidates![i],
+          width: width,
+          answerState: _answerState[i],
+          tap: () {
+            setState(() {
+              for (int j = 0; j < 4; j++) {
+                if (j == i) {
+                  _answerState[j] = true;
+                  _answers[_currentIndex] = j;
+                } else {
+                  _answerState[j] = false;
                 }
-              });
-            }),
+              }
+            });
+          },
+        ),
       );
       _children.add(
         Padding(
