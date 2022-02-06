05e594f510f7753f1caf88c1887cd65da205e4e7
LeeSeokJune <indians1720@gmail.com>
2022-02-06 18:18:08 +0900

    home

--- netflix_practic/lib/screens/home/components/rank_poster.dart
+++ netflix_practic/lib/screens/home/components/rank_poster.dart
@@ -7,6 +7,60 @@ class RankPoster extends StatelessWidget {
   final String posterUrl;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        Positioned(
+          left: 30.0,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(5.0),
+            child: Image(
+              image: AssetImage(posterUrl),
+              fit: BoxFit.cover,
+              width: 120.0,
+              height: 200.0,
+            ),
+          ),
+        ),
+        Positioned(
+          left: -5.0,
+          bottom: -30.0,
+          // 1
+          child: Stack(
+            children: [
+              Text(
+                rank,
+                style: TextStyle(
+                  fontSize: 100.0,
+                  fontWeight: FontWeight.bold,
+                  foreground: Paint()
+                    ..style = PaintingStyle.stroke
+                    ..strokeWidth = 3
+                    ..color = Colors.white,
+                ),
+              ),
+              Text(
+                rank,
+                style: TextStyle(
+                    fontSize: 100.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 150.0,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.black,
+                Colors.black.withOpacity(0.0),
+              ],
+              stops: [0.0, rank != "1" ? 0.2 : 0.0],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
